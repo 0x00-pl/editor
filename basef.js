7fe1e857976352f1c02ef4b22cff40e3ed37409b
pl <0x00.pl@gmail.com>
2013-04-10 23:34:50 +0800

    add namespace

--- basef.js
+++ basef.js
@@ -21,56 +21,82 @@ function escapeHtml(text) {
          .replace(/"/g, "&quot;")
          .replace(/'/g, "&#039;");
  }
-
+function cmf(from, dest, fcollect, fmap, fflip){
+    fcollect= fcollect||function(dest,val){};
+    fmap= fmap||function(x){return x;};
+    fflip= fflip||function(x){return true;};
+    for(i in from){
+	if(fflip(i)){
+	    fcollect(dest,fmap(i));
+	}
+    }
+    return dest;
+}
 
 
 var dataroot={};
 
-//buffer
-function get_curbuffer(){
-    return dataroot.curbuffer;
-}
-function get_buffer(buffer_name){
-    return dataroot.buffers[buffer_name];
-}
-function set_buffer(buffer_name,text){
-    return dataroot.buffers[buffer_name]= text;
-}
-function alloc_buffer(buffer_name){
-    set_buffer(buffer_name, "empty");
-}
-//pipelines
-function pipeline_simple(){
-    this.pool=[];
-    this.render= function(d){
-	var tmp= d;
-	for(i in this.pool){
-	    tmp= i(d);
+buffer={
+    curbuffer:function(){
+	return dataroot.curbuffer;
+    },
+    get_buffer:function(buffer_name){
+	return dataroot.buffers[buffer_name];
+    },
+    set_buffer:function(buffer_name,text){
+	return dataroot.buffers[buffer_name]= text;
+    },
+    alloc_buffer:function(buffer_name){
+	buffer.set_buffer(buffer_name, "empty");
+    },
+};
+vbuf={
+    memdata:function(mem){
+	this.mem= mem;
+	this.toHtml= function(){
+	    return escapeHtml(this.mem);
 	}
-	return tmp;
-    };
-}
-
-function pipeline_echo(){
-    this.render= function(d){
-	return {
-	    text:d,
-	    toHtml:function(){
-		return escapeHtml(this.text);
-	    }
+    },
+    text:function(txt){
+	this.txt= txt;
+	this.toHtml= function(){
+	    return escapeHtml(this.txt);
+	}
+	this.toString= function(){return this.txt;}
+    },
+    split_text:function(vbuf_text,separator){
+	this.token_protype= function(type,val){
+	    this.type= type;
+	    this.val= val;
+	}
+	this.get= function(){
+	    return vbuf_text.toString().split(separator||"")
 	};
-    }
-}
+	this.toHtml= function(){
+	    var ret="";
+	    escapeHtml(cmf(this.tokens, ret, function(r,i){r+=i;}));
+	    return ret;
+	}
+    },
+    html_modify:function(){
+    },
+};
+pipeline={
+    pipeline_echo:function(){
+	this.render= function(d){
+	    return new vbuf.memdata(d);
+	};
+    },
+    render_pipeline:function(pipe,d){
+	var tmp= pipe.render(d);
+	return tmp.toHtml();
+    },
 
-function render_pipeline(pipe,d){
-    var tmp= pipe.render(d);
-    return tmp.toHtml();
-}
-
-function render_curbuffer(){
-    var bf= get_buffer(get_curbuffer());
-    return render_pipeline(dataroot.pipeline, bf);
-}
+    render_curbuffer:function(){
+	var bf= buffer.get_buffer(buffer.curbuffer());
+	return pipeline.render_pipeline(dataroot.pipeline, bf);
+    },
+};
 
 //--------
 
@@ -78,14 +104,11 @@ function init(){
     //buffer
     dataroot.buffers={};
     dataroot.curbuffer="empty";
-    alloc_buffer(dataroot.curbuffer);
+    buffer.alloc_buffer(dataroot.curbuffer);
 
     //pipeline
-    dataroot.pipeline= new pipeline_echo();
+    dataroot.pipeline= new pipeline.pipeline_echo();
 }
 init();
 
-function test(){
-    return render_pipeline(dataroot.pipeline,"132456");
-}
-//alert(test());
+alert("inited");
